676dfabbc3efd8776389715ed34d6cd3169bee26
翁子皓 <x142y857@gmail.com>
2021-06-22 14:18:29 +0800

    6-9

--- main.kt
+++ main.kt
@@ -1,6 +1,12 @@
 fun main(){
-    var beverage = readLine()!!.capitalize()
+    var beverage = readLine()
 
 //	beverage = null
+
+    if (beverage != null){
+        beverage = beverage.capitalize()
+    }else{
+        println("I can't do that without crashing - beverage was null!")
+    }
     println(beverage)
 }
