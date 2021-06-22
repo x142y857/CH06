50894b7d4de6607a984ee369f8431db89559d8e5
翁子皓 <x142y857@gmail.com>
2021-06-22 14:18:00 +0800

    6-8

--- main.kt
+++ main.kt
@@ -1,11 +1,6 @@
 fun main(){
-    var beverage = readLine()?.let{
-        if (it.isNotBlank()){
-            it.capitalize()
-        }else{
-            "Buttered Ale"
-        }
-    }
+    var beverage = readLine()!!.capitalize()
+
 //	beverage = null
     println(beverage)
 }
