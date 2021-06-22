cc37d32ad23a11d002b9ad8aa3c9d3bd4849861c
翁子皓 <x142y857@gmail.com>
2021-06-22 14:20:40 +0800

    6-13

--- main.kt
+++ main.kt
@@ -1,12 +1,18 @@
 fun main(){
     var swordsJuggling: Int? = null
     val isJugglingProficient = (1..3).shuffled().last() == 3
-    if  (isJugglingProficient){
+    if  (isJugglingProficient) {
         swordsJuggling = 2
-    }
 
+    }
+    proficiencyCheck(swordsJuggling)
     swordsJuggling = swordsJuggling!!.plus(1)
 
     println("你雜耍 $swordsJuggling 把劍!")
 
 }
+
+fun proficiencyCheck(swordsJuggling: Int?){
+    swordsJuggling ?: throw IllegalStateException("玩家不能雜耍劍")
+}
+
